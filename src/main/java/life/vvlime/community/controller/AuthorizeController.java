09690a54737b3ab2a58677766f8778d79b37a631
limevv <19805182802@163.com>
2024-09-09 20:50:00 +0800

    add check cookies

--- src/main/java/life/vvlime/community/controller/AuthorizeController.java
+++ src/main/java/life/vvlime/community/controller/AuthorizeController.java
@@ -1,6 +1,9 @@
 package life.vvlime.community.controller;
 
+
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import life.vvlime.community.dto.AccessTokenDTO;
 import life.vvlime.community.dto.GithubUser;
 import life.vvlime.community.mapper.UserMapper;
@@ -11,7 +14,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import java.util.UUID;
 
 @Controller
@@ -32,25 +34,29 @@ public class AuthorizeController {
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
-                           HttpServletRequest request){
+                           HttpServletRequest request,
+                           HttpServletResponse response){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setCode(code);
         accessTokenDTO.setRedirect_uri(redirectUri);
+
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser githubUser = githubProvider.getUser(accessToken);
 
         if(githubUser != null){
             User user = new User();
-            user.setToken(UUID.randomUUID().toString());
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
             user.setName(githubUser.getLogin());
             user.setAccountId(String.valueOf(githubUser.getId()));
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
+
             userMapper.insert(user);
-            //登陆成功，写cookie和session
-            request.getSession().setAttribute("user", githubUser);
+
+            response.addCookie(new Cookie("token", token));
             return "redirect:/";
         }else{
             //登陆失败，重新登录
--- src/main/java/life/vvlime/community/mapper/UserMapper.java
+++ src/main/java/life/vvlime/community/mapper/UserMapper.java
@@ -3,6 +3,8 @@ package life.vvlime.community.mapper;
 import life.vvlime.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 
 @Mapper
@@ -10,4 +12,8 @@ public interface UserMapper {
     @Insert("insert into user (account_id, name, token, gmt_create, gmt_modified) " +
             "values (#{accountId},#{name},#{token},#{gmtCreate},#{gmtModified})")
     void insert (User user);
+
+    @Select("SELECT * FROM user WHERE token = #{token}")
+    User findByToken(@Param("token") String token);
+
 }
